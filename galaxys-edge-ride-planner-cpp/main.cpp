1f4fe2a71db89abe8c3461697f299e09f0231bf4
Luis Navarro <luis.navarro4@snhu.edu>
2026-05-02 11:18:09 -0600

    Add ride priority planning feature

--- galaxys-edge-ride-planner-cpp/main.cpp
+++ galaxys-edge-ride-planner-cpp/main.cpp
@@ -10,6 +10,7 @@ struct Ride {
     string name;
     string land;
     int waitTime;
+    int priority;
 };
 
 void showMenu();
@@ -17,11 +18,13 @@ void viewRides(const vector<Ride>& rides);
 void showTotalWaitTime(const vector<Ride>& rides);
 void suggestBestOrder(vector<Ride> rides);
 void updateWaitTime(vector<Ride>& rides);
+void updatePriority(vector<Ride>& rides);
+string getPriorityLabel(int priority);
 
 int main() {
     vector<Ride> rides = {
-        {"Star Wars: Rise of the Resistance", "Galaxy's Edge", 75},
-        {"Millennium Falcon: Smugglers Run", "Galaxy's Edge", 45}
+        {"Star Wars: Rise of the Resistance", "Galaxy's Edge", 75, 1},
+        {"Millennium Falcon: Smugglers Run", "Galaxy's Edge", 45, 2}
     };
 
     int choice;
@@ -45,13 +48,16 @@ int main() {
             updateWaitTime(rides);
             break;
         case 5:
+            updatePriority(rides);
+            break;
+        case 6:
             cout << "May the Force be with you!\n";
             break;
         default:
             cout << "Invalid option. Try again.\n";
         }
 
-    } while (choice != 5);
+    } while (choice != 6);
 
     return 0;
 }
@@ -62,7 +68,8 @@ void showMenu() {
     cout << "2. Show total wait time\n";
     cout << "3. Suggest best ride order\n";
     cout << "4. Update wait time\n";
-    cout << "5. Exit\n";
+    cout << "5. Update ride priority\n";
+    cout << "6. Exit\n";
     cout << "Choose an option: ";
 }
 
@@ -73,6 +80,7 @@ void viewRides(const vector<Ride>& rides) {
         cout << i + 1 << ". " << rides[i].name << endl;
         cout << "   Land: " << rides[i].land << endl;
         cout << "   Wait Time: " << rides[i].waitTime << " minutes\n";
+        cout << "   Priority: " << getPriorityLabel(rides[i].priority) << endl;
     }
 }
 
@@ -88,14 +96,18 @@ void showTotalWaitTime(const vector<Ride>& rides) {
 
 void suggestBestOrder(vector<Ride> rides) {
     sort(rides.begin(), rides.end(), [](const Ride& a, const Ride& b) {
-        return a.waitTime < b.waitTime;
+        if (a.priority == b.priority) {
+            return a.waitTime < b.waitTime;
+        }
+        return a.priority < b.priority;
         });
 
     cout << "\n===== Suggested Ride Order =====\n";
 
     for (size_t i = 0; i < rides.size(); i++) {
         cout << i + 1 << ". " << rides[i].name
-            << " - " << rides[i].waitTime << " minutes\n";
+            << " - " << rides[i].waitTime << " minutes"
+            << " - " << getPriorityLabel(rides[i].priority) << endl;
     }
 }
 
@@ -108,7 +120,7 @@ void updateWaitTime(vector<Ride>& rides) {
     cout << "\nWhich ride would you like to update? ";
     cin >> rideChoice;
 
-    if (rideChoice < 1 || rideChoice > rides.size()) {
+    if (rideChoice < 1 || rideChoice > static_cast<int>(rides.size())) {
         cout << "Invalid ride selection.\n";
         return;
     }
@@ -119,4 +131,48 @@ void updateWaitTime(vector<Ride>& rides) {
     rides[rideChoice - 1].waitTime = newWaitTime;
 
     cout << "Wait time updated successfully.\n";
+}
+
+void updatePriority(vector<Ride>& rides) {
+    int rideChoice;
+    int newPriority;
+
+    viewRides(rides);
+
+    cout << "\nWhich ride would you like to update priority for? ";
+    cin >> rideChoice;
+
+    if (rideChoice < 1 || rideChoice > static_cast<int>(rides.size())) {
+        cout << "Invalid ride selection.\n";
+        return;
+    }
+
+    cout << "\nPriority options:\n";
+    cout << "1. Must Ride\n";
+    cout << "2. Nice to Ride\n";
+    cout << "3. Optional\n";
+    cout << "Choose priority: ";
+    cin >> newPriority;
+
+    if (newPriority < 1 || newPriority > 3) {
+        cout << "Invalid priority.\n";
+        return;
+    }
+
+    rides[rideChoice - 1].priority = newPriority;
+
+    cout << "Priority updated successfully.\n";
+}
+
+string getPriorityLabel(int priority) {
+    switch (priority) {
+    case 1:
+        return "Must Ride";
+    case 2:
+        return "Nice to Ride";
+    case 3:
+        return "Optional";
+    default:
+        return "Unknown";
+    }
 }
